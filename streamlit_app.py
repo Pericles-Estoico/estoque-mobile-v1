dc89d976f4d616eef465338450f5954363a6432a
Pericles-Estoico <finaarteenxovais@yahoo.com>
2025-09-24 14:37:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.set_page_config(
 
 # URLs
 SHEETS_URL = "https://docs.google.com/spreadsheets/d/1PpiMQingHf4llA03BiPIuPJPIZqul4grRU_emWDEK1o/export?format=csv"
-WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzCxotWn-SXG52CXU7tNnd7KtBhx1uYwHr-ka2qWjswTcfj3QvHuA1VvDo-BL_fpg8U/exec"
+WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbztcleBJ2hsCedvExXIZy5AsXTYknhd4L83BEUWfBswFKgKOXmgMHCAeR9P9Jgmwj9_/exec"
 
 # Função para carregar produtos
 @st.cache_data(ttl=30)
